06ab007ca06fd59a27d7f7de8ad7b44697af2ce4
Everton Vieira <evertonvieira1994@gmail.com>
2018-03-15 22:00:50 -0300

    adicionando estrutura da tela inicial

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const { app, BrowserWindow } = require("electron");
 
 app.on("ready", () => {
 
-    let MyWindow = new BrowserWindow({
+    let HomeWindow = new BrowserWindow({
         width: 450,
         height: 250
     });
+
+    HomeWindow.loadURL(`file://${__dirname}/app/views/index.html`);
 
 });
 
